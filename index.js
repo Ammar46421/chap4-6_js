3a8fef4bcd295a23a971ae04642a0354ef25665b
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-03-10 22:01:59 +0500

    summary

---
+++ index.js
@@ -0,0 +1,20 @@
+// chap 4
+var user = 'hi';
+var viewer = 'yes?'; // if we write the User instead of  user it wil be illegal
+
+var add = user + ' how are you'; // now it will show the value of user + the value entered 
+
+
+var house = 'House# 22';
+var street = house + ',Street 3rd';
+
+
+// chap 5
+var model = 2019;
+var enginepower = (12*11)-9;
+var upgrade = enginepower + (5*24)/2;
+
+// chap 6
+var age = 22
+var nextyear = age++ //it still shows the same number
+var add = nextyear + 2
